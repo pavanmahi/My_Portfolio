d53bb4c53ae2a288e4e1d504e67e3c10a73f4e0c
pavan <pavanbejawada4376@gmail.com>
2025-08-27 18:26:53 +0530

    portfolio

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";

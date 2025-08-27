d04cb0f42a0f3bf588a1f847ecd5670d8dbeefc1
pavan <pavanbejawada4376@gmail.com>
2025-08-27 18:39:20 +0530

    Portfolio

--- app/page.js
+++ app/page.js
@@ -1,18 +1,72 @@
+// "use client";
+
+// import AboutSection from "./components/homepage/about";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Certifications from "./components/homepage/certifications";
+// import Activities from "./components/homepage/activities";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
+
+// export default async function Home() {
+//   return (
+//     <div suppressHydrationWarning >
+//       <HeroSection />
+//       <AboutSection />
+//       <Experience />
+//       <Skills />
+//       <Projects />
+//       <Education />
+//       <Certifications />
+//       <Activities />
+//       <ContactSection />
+//     </div>
+//   )
+// };
+
 "use client";
 
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Certifications from "./components/homepage/certifications";
-import Activities from "./components/homepage/activities";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+import dynamic from "next/dynamic";
 
-export default async function Home() {
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), {
+  ssr: false,
+});
+const AboutSection = dynamic(() => import("./components/homepage/about"), {
+  ssr: false,
+});
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
+const Skills = dynamic(() => import("./components/homepage/skills"), {
+  ssr: false,
+});
+const Projects = dynamic(() => import("./components/homepage/projects"), {
+  ssr: false,
+});
+const Education = dynamic(() => import("./components/homepage/education"), {
+  ssr: false,
+});
+const Certifications = dynamic(
+  () => import("./components/homepage/certifications"),
+  {
+    ssr: false,
+  }
+);
+const Activities = dynamic(() => import("./components/homepage/activities"), {
+  ssr: false,
+});
+const ContactSection = dynamic(
+  () => import("./components/homepage/contact"),
+  {
+    ssr: false,
+  }
+);
+
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -23,5 +77,5 @@ export default async function Home() {
       <Activities />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
